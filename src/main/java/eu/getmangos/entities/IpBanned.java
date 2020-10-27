0a29004fd04c88128329f8d29bc1aa44c957a845
Warkdev <cedric.servais@outlook.com>
2020-10-27 22:27:51 +0100

    Update Named Query findByIp

--- src/main/java/eu/getmangos/entities/IpBanned.java
+++ src/main/java/eu/getmangos/entities/IpBanned.java
@@ -19,7 +19,7 @@ import javax.validation.constraints.NotNull;
 @NamedQueries({
     @NamedQuery(name = "IpBanned.findAll", query = "SELECT i FROM IpBanned i"),
     @NamedQuery(name = "IpBanned.findById", query = "SELECT i FROM IpBanned i WHERE i.IpBanned = :id"),
-    @NamedQuery(name = "IpBanned.findByAccount", query = "SELECT i FROM IpBanned i WHERE i.IpBanned.id = :id")
+    @NamedQuery(name = "IpBanned.findByIp", query = "SELECT i FROM IpBanned i WHERE i.IpBanned.ip = :id")
 })
 public class IpBanned implements Serializable {
 
