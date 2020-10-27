67b32b8c77e4c02adb7c52c45648aed314d1a3d2
Warkdev <cedric.servais@outlook.com>
2020-10-27 22:25:36 +0100

    Remove unused imports

--- src/main/java/eu/getmangos/rest/DbVersionRest.java
+++ src/main/java/eu/getmangos/rest/DbVersionRest.java
@@ -1,19 +1,11 @@
 package eu.getmangos.rest;
 
-import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.Operation;
