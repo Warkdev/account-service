98f843a89687008940ba902152fade35cccff23a
Warkdev <cedric.servais@outlook.com>
2020-11-24 20:42:26 +0100

    Update database name

--- scripts/database/structure.sql
+++ scripts/database/structure.sql
@@ -14,6 +14,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE  IF NOT EXISTS `auth` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `auth`;
+
 --
 -- Table structure for table `account`
 --
